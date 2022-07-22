feaee3b4cc15d29404dd4df7bce36a09c76d82a7
张慧 <1772764331@qq.com>
2022-07-22 23:48:36 +0800

    myGreeter

--- javascript/test/my_greeter-client.js
+++ javascript/test/my_greeter-client.js
@@ -1,25 +1,32 @@
 'use strict';
 
-var assert = require('assert');
+/**
+ * @doc :https://mochajs.cn/
+ * @author：zh
+ */
+var assert = require('chai').assert
 var MyGreeter = require('../src/MyGreeter.js');
 
-describe('MyGreeter.Client', function () {
+describe('MyGreeter', function () {
 
     describe('Constructor', function () {
-
         it('should be instantiated', function() {
             var greeter = new MyGreeter.Client();
-
-            assert.ok(greeter instanceof MyGreeter.Client);
+            assert.equal(greeter instanceof MyGreeter.Client, true);
         });
     });
 
     describe('getGreeting', function () {
-
-        it('should return something with length', function() {
-            var greeter = new MyGreeter.Client();
-
-            assert.equal(greeter.getGreeting().length > 0, true);
+        it('getGreeting结果验证', function() {
+            var greeter = new MyGreeter.Client(10);
+            assert.typeOf(greeter.getGreeting, 'function','getGreeting方法不合法');
+            assert.equal(!!greeter.getGreeting(), true, '返回值为空');
+            assert.equal(greeter.getGreeting(), 'Good morning','返回内容不正确');
         });
     });
+    
 });
+
+
+
+
